315b05760eecd360b9a9a2a02ba1776ad057046c
Petr E. Antonov <valumar@users.noreply.github.com>
2017-03-15 10:46:13 +0000

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
